25874dd064eef74736a2cb1244a346c2b107254d
MohamedCS <you@example.com>
2022-01-22 16:24:48 +0100

    first commit

---
+++ app/src/main/java/com/example/govtech/presentation.kt
@@ -0,0 +1,13 @@
+package com.example.govtech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class presentation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_presentation)
+    }
+
+}
