fb980d69d07ce9d1410a6f7c23df7c5e5f82338b
MohamedCS <you@example.com>
2022-01-22 22:52:29 +0100

    add fragments

--- app/src/main/java/com/example/govtech/LoginActivity.kt
+++ app/src/main/java/com/example/govtech/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.govtech
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    var bu_skip:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        bu_skip = findViewById(R.id.bu_loginskip)
+
+        bu_skip!!.setOnClickListener {
+            startActivity(Intent(this ,MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/govtech/MainActivity.kt
+++ app/src/main/java/com/example/govtech/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.govtech
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragments.ChatFragment
+import com.example.fragments.HomeFragment
+import com.example.fragments.OrganisateurFragment
+import com.example.fragments.SettingsFragment
+import io.ak1.BubbleTabBar
+import io.ak1.OnBubbleClickListener
 
 class MainActivity : AppCompatActivity() {
+    var bottomtabBar:BubbleTabBar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomtabBar = findViewById(R.id.bubbleTabBar)
+
+        bottomtabBar!!.addBubbleListener(object :OnBubbleClickListener{
+            override fun onBubbleClick(id: Int) {
+                val fm = supportFragmentManager
+                val ft = fm.beginTransaction()
+                when(id)
+                {
+                    R.id.home->{
+                        ft.replace(R.id.fragmentContainerView ,HomeFragment())
+                        ft.commit()
+                    }
+                    R.id.org ->{
+                        ft.replace(R.id.fragmentContainerView ,OrganisateurFragment())
+                        ft.commit()
+                    }
+                    R.id.chat->{
+                        ft.replace(R.id.fragmentContainerView ,ChatFragment())
+                        ft.commit()
+                    }
+                    R.id.settings->{
+                        ft.replace(R.id.fragmentContainerView ,SettingsFragment())
+                        ft.commit()
+                    }
+                }
+            }
+
+        })
     }
 }
